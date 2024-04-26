f0e78438aeae0717e648aa4183aada896642a26e
poorvi shukla <shuklapoorvi26@gmail.com>
2024-04-26 13:22:43 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,19 @@
 function longestSubstring(s) {
-
-
-    // Implementation of longestSubstring function
-}
-
+    // var lengthOfLongestSubstring = function(s) {
+        let left = 0;
+        let maxLength = 0;
+        let charSet = new Set();
+    
+        for (let right = 0; right < s.length; right++) {
+            while (charSet.has(s[right])) {
+                charSet.delete(s[left]);
+                left++;
+            }
+    
+            charSet.add(s[right]);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+    
+        return maxLength;    
+    };
 module.exports = { longestSubstring };
