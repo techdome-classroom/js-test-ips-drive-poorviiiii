776aafeb3af92483d043d7e8d4e55d600ce4ce4d
poorvi shukla <shuklapoorvi26@gmail.com>
2024-04-26 13:15:07 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,9 +1,20 @@
 function smallestMissingPositiveInteger(nums) {
-
-
-    // Implement the function smallest_missing_positive_integer
-  
+  let n=nums.length;
+  let present=new Array(n+1);
+  for(let i=0;i<n+1;i++){
+    present[i]=false;
   }
-  
-  module.exports = smallestMissingPositiveInteger;
+  for(let i=0;i<n;i++){
+    if(nums[i]>0 && nums[i]<=n){
+      present[nums[i]]=true;
+    }
+  }
+  for(let i=1;i<=n;i++){
+      if(!present[i]){
+        return i;
+      }
+  }
+  return n+1;
+}
+module.exports = smallestMissingPositiveInteger;
 
